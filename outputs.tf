a9df0ad72fb375cf52284ea0589a7664b71e7b36
Clayton Bailey <cbailey6079@gmail.com>
2024-04-30 14:52:46 -0600

    replacing tolist to values

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "ecr_repository_urls" {
-  value       = tolist(aws_ecr_repository.repos)[0]
+  value       = values(aws_ecr_repository.repos)[0]
   description = "A list of urls of the repositories that were created."
 }
 
 output "ecr_repository_urls2" {
-  value       = tolist(aws_ecr_repository.repos)[0].repository_url
+  value       = values(aws_ecr_repository.repos)[0].repository_url
   description = "A list of urls of the repositories that were created."
 }
