204d1bd6f12a2d824c51010c8fc1d6d77175b58a
Clayton Bailey <cbailey6079@gmail.com>
2024-04-30 14:47:00 -0600

    testing to listing the repo output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "ecr_repository_urls" {
-  value       = aws_ecr_repository.repos[0]
+  value       = tolist(aws_ecr_repository.repos)[0]
   description = "A list of urls of the repositories that were created."
 }
 
 output "ecr_repository_urls2" {
-  value       = aws_ecr_repository.repos[0].repository_url
+  value       = tolist(aws_ecr_repository.repos)[0].repository_url
   description = "A list of urls of the repositories that were created."
 }
