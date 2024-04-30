a98f224eec2074085679ca4c598a09fc9763c0e2
Clayton Bailey <cbailey6079@gmail.com>
2024-04-30 15:35:59 -0600

    moving the repo policy to a template and local variables

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,14 @@
 locals {
-  lifecycle_policy    = templatefile("${path.module}/templates/lifecycle_policy.tftpl", { images_to_keep = var.images_to_keep })
-  push_allowed_policy = templatefile("${path.module}/templates/push_policy.tftpl", { push_account_list = "${join(",", formatlist("\"arn:aws:iam::%s:root\"", var.push_account_list))}" })
-  pull_allowed_policy = templatefile("${path.module}/templates/pull_policy.tftpl", { pull_account_list = "${join(",", formatlist("\"arn:aws:iam::%s:root\"", var.pull_account_list))}" })
+  push_account_list = join(",", formatlist("\"arn:aws:iam::%s:root\"", var.push_account_list))
+  pull_account_list = join(",", formatlist("\"arn:aws:iam::%s:root\"", var.pull_account_list))
+  lifecycle_policy  = templatefile("${path.module}/templates/lifecycle_policy.tftpl", { images_to_keep = var.images_to_keep })
+  push_policy       = templatefile("${path.module}/templates/push_policy.tftpl", { push_account_list = local.push_account_list })
+  pull_policy       = templatefile("${path.module}/templates/pull_policy.tftpl", { pull_account_list = local.pull_account_list })
+  repo_policy_statement = (join(",", compact(tolist([
+    length(var.pull_account_list) == 0 ? "" : local.pull_policy,
+    length(var.push_account_list) == 0 ? "" : local.push_policy
+  ]))))
+  repo_policy = templatefile("${path.module}/templates/repo_policy.tftpl", { repo_policy_statement = local.repo_policy_statement })
 }
 
 resource "aws_ecr_repository" "repos" {
@@ -18,22 +25,11 @@ resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
   policy = local.lifecycle_policy
 }
 
-# This is the stupidest terraform I've ever had to write. Good lord kill me.
 resource "aws_ecr_repository_policy" "policy" {
   for_each   = var.repository_list
   repository = each.value
 
   depends_on = [aws_ecr_repository.repos]
 
-  policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-      ${join(",", compact(tolist([
-  length(var.pull_account_list) == 0 ? "" : local.pull_allowed_policy,
-  length(var.push_account_list) == 0 ? "" : local.push_allowed_policy
-])))}
-    ]
-}
-EOF
+  policy = local.repo_policy
 }
