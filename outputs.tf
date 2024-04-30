b8360eafe5eea9f6eaa63de50155c80b2970ae93
Clayton Bailey <cbailey6079@gmail.com>
2024-04-30 14:43:31 -0600

    trying to move some logic into the template files, also adding additional outputs for debuging

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
 output "ecr_repository_urls" {
-  value       = aws_ecr_repository.repos[*].repository_url
+  value       = aws_ecr_repository.repos[0]
   description = "A list of urls of the repositories that were created."
-  depends_on = [ aws_ecr_repository.repos ]
+}
+
+output "ecr_repository_urls2" {
+  value       = aws_ecr_repository.repos[0].repository_url
+  description = "A list of urls of the repositories that were created."
 }
