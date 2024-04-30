8c43efebac60fe2145f6c782fc94365c986af035
Clayton Bailey <cbailey6079@gmail.com>
2024-04-30 14:58:02 -0600

    fixing the output for repository urls

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 output "ecr_repository_urls" {
-  value       = values(aws_ecr_repository.repos)[0]
-  description = "A list of urls of the repositories that were created."
-}
-
-output "ecr_repository_urls2" {
-  value       = values(aws_ecr_repository.repos)[0].repository_url
+  value       = values(aws_ecr_repository.repos)[*].repository_url
   description = "A list of urls of the repositories that were created."
 }
