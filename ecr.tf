c05f7c72ded44518bfe2917e82b50d26e95cca62
Clayton Bailey <cbailey6079@gmail.com>
2024-05-01 08:29:07 -0600

    removing unnecessary parens

--- ecr.tf
+++ ecr.tf
@@ -4,10 +4,10 @@ locals {
   lifecycle_policy  = templatefile("${path.module}/templates/lifecycle_policy.tftpl", { images_to_keep = var.images_to_keep })
   push_policy       = templatefile("${path.module}/templates/push_policy.tftpl", { push_account_list = local.push_account_arn })
   pull_policy       = templatefile("${path.module}/templates/pull_policy.tftpl", { pull_account_list = local.pull_account_arn })
-  repo_policy_statement = (join(",", compact(tolist([
+  repo_policy_statement = join(",", compact(tolist([
     length(var.pull_account_list) == 0 ? "" : local.pull_policy,
     length(var.push_account_list) == 0 ? "" : local.push_policy
-  ]))))
+  ])))
   repo_policy = templatefile("${path.module}/templates/repo_policy.tftpl", { repo_policy_statement = local.repo_policy_statement })
 }
 
